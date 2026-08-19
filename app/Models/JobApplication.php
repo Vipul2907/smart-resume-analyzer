1b540348b510e9fbb4960e7a45eae5b7cef9cac2
dipu <diputhakor2711@gmail.com>
2026-08-20 00:26:19 +0530

    Add JobApplication model

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JobApplication extends Model
@@ -15,5 +16,26 @@ class JobApplication extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(JobContact::class);
+    }
+
+    public function attachments(): HasMany
+    {
+        return $this->hasMany(JobAttachment::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'applied_at' => 'date',
+            'application_date' => 'date',
+            'follow_up_at' => 'date',
+            'deadline' => 'date',
+            'priority' => 'integer',
+        ];
     }
 }
