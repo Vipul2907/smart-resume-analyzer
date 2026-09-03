339efd8b93fe6b49c9b1dbfbd8e9ebf8d888007d
dipu <diputhakor2711@gmail.com>
2026-09-03 20:58:47 +0530

    feat: update career profile for public portfolio

--- app/Models/CareerProfile.php
+++ app/Models/CareerProfile.php
@@ -9,6 +9,16 @@ class CareerProfile extends Model
 {
     protected $guarded = [];
 
+    protected function casts(): array
+    {
+        return [
+            'available_for_work' => 'boolean',
+            'portfolio_is_public' => 'boolean',
+            'show_contact_email' => 'boolean',
+            'show_resume' => 'boolean',
+        ];
+    }
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
